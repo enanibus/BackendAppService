c29a0f134b9c1952f57e68022334c0178e12018b
Jacobo Enriquez <michi_two@yahoo.comn>
2016-10-27 20:04:13 +0200

    Insert en table Post, tabla corregida

--- tables/Posts.js
+++ tables/Posts.js
@@ -10,14 +10,14 @@ table.columns = {
     "titulo" : "string",
     "texto" : "string",
     "foto" : "string"
-    // "latitud" : "number",
-    // "longitud" : "number",
-    // "autor" : "string",
-    // "publicado" : "boolean",
-    // "valoracion" : "number",
-    // "numOfVals" : "number",
-    // "paraPublicar" : "boolean",
-    // "container" : "string"
+    "latitud" : "number",
+    "longitud" : "number",
+    "autor" : "string",
+    "publicado" : "boolean",
+    "valoracion" : "number",
+    "numOfVals" : "number",
+    "paraPublicar" : "boolean",
+    "container" : "string"
 };
 
 // table.dynamicSchema = false;
@@ -26,16 +26,9 @@ table.columns = {
  Trigger para insert
  */
 
-// table.insert(function (context) {
-//     context.item.idUsuario = context.user.id;
-//     return context.execute();
-// });
-
-
-table.insert(function(context, request) {
-    console.log("Saving new News");
-    console.log(item.description);
-    // Setting default values
+table.insert(function (context) {
+    context.item.idUsuario = context.user.id;
+    //     // Setting default values
     context.item.longitud = 0
     context.item.latitud = 0
     context.item.publicado = false
@@ -43,45 +36,60 @@ table.insert(function(context, request) {
     context.item.numOfVals = 0
     context.item.paraPublicar = true
     context.item.container = ""
-
-    var error = false
-    if(context.item.titulo === ""){
-        request.respond(statusCodes.badRequest, 'Text must be required');
-        error = true;
-    }
-    if(context.item.texto.length < 10){
-        request.respond(statusCodes.badRequest,
-            'Body length must be more than 10 characters');
-        error = true;
-    }
-    // if(item.foto === ""){
-    //     request.respond(statusCodes.BAD_REQUEST, 'Photo must be required');
-    //     error = true;
-    // }
-
-    if (!error){
-            context.item.idUsuario = context.user.id;
-            console.log("Insertando post de usuario: " + context.user.id);
-            return context.execute();
-        // context.user.getIdentities({
-        //     success: function (identities) {
-        //         var http = require('request');
-        //         console.log('Identities: ', identities);
-        //         var url = 'https://graph.facebook.com/me?fields=id,name,birthday,hometown,email,picture,gender,friends&access_token=' +
-        //             identities.facebook.accessToken;
-        //
-        //         var reqParams = { uri: url, headers: { Accept: 'application/json' } };
-        //         http.get(reqParams, function (err, resp, body) {
-        //             var userData = JSON.parse(body);
-        //             console.log('Logado -> ' + userData.name);
-        //             item.id = userData.id;
-        //             item.autor = userData.name
-        //             request.execute();
-        //         });
-        //     }
-        // });
-    }
+    return context.execute();
 });
+
+
+// table.insert(function(context, request) {
+//     console.log("Saving new News");
+//     console.log(item.description);
+//     // Setting default values
+//     context.item.longitud = 0
+//     context.item.latitud = 0
+//     context.item.publicado = false
+//     context.item.valoracion = 0
+//     context.item.numOfVals = 0
+//     context.item.paraPublicar = true
+//     context.item.container = ""
+//
+//     var error = false
+//     if(context.item.titulo === ""){
+//         request.respond(statusCodes.badRequest, 'Text must be required');
+//         error = true;
+//     }
+//     if(context.item.texto.length < 10){
+//         request.respond(statusCodes.badRequest,
+//             'Body length must be more than 10 characters');
+//         error = true;
+//     }
+//     // if(item.foto === ""){
+//     //     request.respond(statusCodes.BAD_REQUEST, 'Photo must be required');
+//     //     error = true;
+//     // }
+//
+//     if (!error){
+//             context.item.idUsuario = context.user.id;
+//             console.log("Insertando post de usuario: " + context.user.id);
+//             return context.execute();
+//         // context.user.getIdentities({
+//         //     success: function (identities) {
+//         //         var http = require('request');
+//         //         console.log('Identities: ', identities);
+//         //         var url = 'https://graph.facebook.com/me?fields=id,name,birthday,hometown,email,picture,gender,friends&access_token=' +
+//         //             identities.facebook.accessToken;
+//         //
+//         //         var reqParams = { uri: url, headers: { Accept: 'application/json' } };
+//         //         http.get(reqParams, function (err, resp, body) {
+//         //             var userData = JSON.parse(body);
+//         //             console.log('Logado -> ' + userData.name);
+//         //             item.id = userData.id;
+//         //             item.autor = userData.name
+//         //             request.execute();
+//         //         });
+//         //     }
+//         // });
+//     }
+// });
 
 
 // table.read(function (context) {

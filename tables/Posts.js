fb4d60d12165c601cd7eb22dc38ffdfbad40a242
Jacobo Enriquez <michi_two@yahoo.comn>
2016-10-27 22:27:02 +0200

    Insert en table Post, tabla corregida

--- tables/Posts.js
+++ tables/Posts.js
@@ -54,7 +54,33 @@ table.insert(function (context) {
     //     error = true;
     // }
 
+    //return context.execute();
+
+    if (!error){
+        //context.item.idUsuario = context.user.id;
+        console.log("Insertando post de usuario: " + context.user.id);
+        context.user.getIdentities({
+            success: function (identities) {
+                var http = require('request');
+                console.log('Identities: ', identities);
+                var url = 'https://graph.facebook.com/me?fields=id,name,birthday,hometown,email,picture,gender,friends&access_token=' +
+                        identities.facebook.accessToken;
+
+                var reqParams = { uri: url, headers: { Accept: 'application/json' } };
+                http.get(reqParams, function (err, resp, body) {
+                    var userData = JSON.parse(body);
+                    console.log('Logado -> ' + userData.name);
+                    context.item.id = userData.id;
+                    context.item.autor = userData.name
+                    context.req.execute();
+                });
+            }
+        });
+    }
+
     return context.execute();
+
+
 });
 
 

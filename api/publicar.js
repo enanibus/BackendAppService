fcf5a1feb48c774bce11a7eb9124e115d8da49b0
Jacobo Enriquez <michi_two@yahoo.comn>
2016-10-30 15:17:19 +0100

    Creado job para publicar Posts (publicar.js)

--- api/publicar.js
+++ api/publicar.js
@@ -2,8 +2,21 @@
  * Created by jacobo on 30/10/16.
  */
 
-var azureMobile = require('azure-mobile-apps');
+var api = {
+    put: function (req, res, next) {
+
+        if (typeof  req.params.length < 0) {
+            console.log("error")
+        }
+        // sql: "Select * FROM Posts WHERE paraPublicar = '" 1 "'"
+        // var querySelect = {
+        //     sql: "Select * FROM Posts"
+        // };
+
+        // req.azureMobile.data.execute(querySelect)
+        //     .then(function (result) {
                     var queryUpdate = {
+                        // sql: "UPDATE Posts SET paraPublicar=" + false + ", publicado=" + true + " WHERE paraPublicar = " + true + ""
                         sql: 'UPDATE Posts SET paraPublicar=@pendientes, publicado=@publica WHERE paraPublicar=@pendiente',
                             parameters: [
                                 {name: 'pendientes' , value : false},
@@ -12,7 +25,15 @@ var azureMobile = require('azure-mobile-apps');
                             ]
                     };
                     console.log("Ejecutando UPDATE de JOB de Publicación")
-                    azureMobile.data.execute(queryUpdate)
+                    req.azureMobile.data.execute(queryUpdate)
                         .then(function (result) {
                             res.json("-----------Ejecutado JOB de Publicación------------")
                         });
+            //     }
+            // );
+    }
+};
+
+api.put.access = 'authenticated';
+
+module.exports = api;

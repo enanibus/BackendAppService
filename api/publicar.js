8ba624d8beba6dc07a8bdfe60902ff4507d4206e
Jacobo Enriquez <michi_two@yahoo.comn>
2016-10-30 14:17:53 +0100

    Creado job para publicar Posts (publicar.js)

--- api/publicar.js
+++ api/publicar.js
@@ -16,7 +16,13 @@ var api = {
         req.azureMobile.data.execute(querySelect)
             .then(function (result) {
                     var queryUpdate = {
-                        sql: "UPDATE Posts SET paraPublicar=" + false + ", publicado=" + true + " WHERE paraPublicar = " + true + ""
+                        // sql: "UPDATE Posts SET paraPublicar=" + false + ", publicado=" + true + " WHERE paraPublicar = " + true + ""
+                        sql: 'UPDATE Posts SET paraPublicar=@pendientes, publicado=@publica WHERE paraPublicar=@pendientePublicar',
+                        parameters: [{
+                            pendientes: false,
+                            publica: true,
+                            pendientePublicar: true
+                        }]
                     };
                     req.azureMobile.data.execute(queryUpdate)
                         .then(function (result) {
